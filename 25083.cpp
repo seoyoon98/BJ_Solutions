d59e484b27b73a40c03013e99ef3e62ee5943d3c
seoYoon <seoyuni0727@gmail.com>
2023-01-14 02:59:19 +0900

    Create 25083.cpp
    Sol) no.25083

---
+++ 25083.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    /*
+     ,r'"7
+     r`-_   ,'  ,/
+     \. ". L_r'
+     `~\/
+     |
+     |
+     }
+     */
+    
+    cout << "         ,r'\"7" << "\n";
+    cout << "r`-_   ,'  ,/" << "\n";
+    cout << " \\. \". L_r'" << "\n";
+    cout << "   `~\\/" << "\n";
+    cout << "      |" << "\n";
+    cout << "      |" << "\n";
+}
