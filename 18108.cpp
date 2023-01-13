9fc52a49bb0b56684286352071dcbc7c7d7b23c5
seoYoon <seoyuni0727@gmail.com>
2023-01-14 02:34:44 +0900

    Create 18108.cpp
    Sol) no.18108

---
+++ 18108.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int thaiY;
+    int diffY = 2541-1998;
+    
+    cin >> thaiY;
+    cout << thaiY - diffY << "\n";
+    return 0;
+}
